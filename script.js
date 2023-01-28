d4b1872f04a89a97be7c4713fe2a2bede7bf7ec2
spiffykai <kaidoldan@gmail.com>
2023-01-27 23:50:03 -0500

    Added decimal and negative functionality

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
 const digitBar = document.getElementById("display");
 const numberButtons = document.querySelectorAll(".numberButton");
 const operatorButtons = document.querySelectorAll(".operationButton");
+const decimalButton = document.getElementById("decimalButton");
+const negativeButton = document.getElementById("negativeButton");
 
 let currentNumberShown = "0";
 digitBar.innerText = currentNumberShown;
 
 let value1 = 0.0;
 let value2 = 0.0;
+let isValue1 = true;
+let isValue2 = false;
 let decimalUsed = false;
+let isNegative = false;
 
 let currentOperation = "+";
+
+decimalButton.addEventListener("click", function(){
+    if(!decimalUsed){
+        if (currentNumberShown == "0" && currentNumberShown.length < 10) {
+            currentNumberShown = ".";
+            decimalUsed = true;
+        } else if (currentNumberShown.length < 10) {
+            currentNumberShown += ".";
+            decimalUsed = true;
+        }
+        digitBar.innerText = currentNumberShown;
+    }
+});
+
+negativeButton.addEventListener("click", function(){
+    if (!isNegative){
+        if(currentNumberShown = "0"){
+            currentNumberShown = "-";
+        } else {
+            currentNumberShown = "-" + currentNumberShown;
+        }
+        isNegative = true;
+    } else if (isNegative){
+        currentNumberShown = currentNumberShown.replace("-", '');
+        isNegative = false;
+    }
+    digitBar.innerText = currentNumberShown;
+});
 
 for (let x in numberButtons){
     if (numberButtons[x].nodeName == "BUTTON"){
@@ -31,7 +64,10 @@ for (let x in operatorButtons) {
             currentOperation = operatorButtons[x].innerText;
             value1 = parseFloat(currentNumberShown);
             currentNumberShown = "0";
+            decimalUsed = false;
             //digitBar.innerText = currentNumberShown;
+            isValue1 = false;
+            isValue2 = true;
             value2 = 0;
         });
     }
@@ -39,6 +75,7 @@ for (let x in operatorButtons) {
 
 document.getElementById("equalsButton").addEventListener("click", function(){
     operation(currentOperation);
+    decimalUsed = false;
 });
 
 function operation(operationToUse){
@@ -65,6 +102,8 @@ function operation(operationToUse){
     value1 = parseFloat(currentNumberShown);
     value2 = 0;
     digitBar.innerText = currentNumberShown;
+    isValue1 = true;
+    isValue2 = false;
 }
 
 document.getElementById("clearButton").addEventListener("click", function(){
@@ -72,4 +111,7 @@ document.getElementById("clearButton").addEventListener("click", function(){
     value2 = 0;
     currentNumberShown = "0";
     digitBar.innerText = currentNumberShown;
+    decimalUsed = false;
+    isValue1 = true;
+    isValue2 = false;
 });
